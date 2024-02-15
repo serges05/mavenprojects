4300cf75c7a258d463d413954e243309947e0feb
serges05 <serges05@yahoo.fr>
2024-02-15 01:16:23 +0100

    my first maven projects

---
+++ calbe/src/main/java/Nks_/calbe/CalbeApplication.java
@@ -0,0 +1,13 @@
+package Nks_.calbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalbeApplication.class, args);
+	}
+
+}
---
+++ calbe/src/test/java/Nks_/calbe/CalbeApplicationTests.java
@@ -0,0 +1,13 @@
+package Nks_.calbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
